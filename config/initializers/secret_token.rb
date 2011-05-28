20e9e7d43a41eff696b262dbe66422f7277b0bf8
Phil Goldsbrough <phil.goldsbrough@btinternet.com>
2011-05-28 13:43:18 +0100

    New Rails 3 project structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrombyAM::Application.config.secret_token = '01f2fdb16ec7628215a6c03725930e1dea1f16f685771f81a030fb1865382c3554d723e831a5b098e32a41f038dca5403b73ac81c20c4be3d35f74762b98730a'
